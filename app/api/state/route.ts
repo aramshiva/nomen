f5a07e7701e77d8b00f121e6dce2c6cc1f7d88b4
Aram <me@aram.sh>
2025-03-17 19:18:38 -0700

    prettier + state API

--- app/api/state/route.ts
+++ app/api/state/route.ts
@@ -1,37 +1,44 @@
 import { db } from "@/lib/db";
-import { names } from "@/lib/schema";
+import { namesbyarea } from "@/lib/schema";
 import { eq, and, asc } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
-  try {
-    const { searchParams } = new URL(request.url);
-    const name = searchParams.get("name");
-    const sex = searchParams.get("sex");
+    try {
+        const { searchParams } = new URL(request.url);
+        const name = searchParams.get("name");
+        const sex = searchParams.get("sex");
+        const state = searchParams.get("state");
 
-    if (!name) {
-      return NextResponse.json(
-        { error: "Name parameters are required" },
-        { status: 400 },
-      );
+        if (!name) {
+            return NextResponse.json(
+                { error: "Name parameters are required" },
+                { status: 400 },
+            );
+        }
+
+        let conditions = eq(namesbyarea.name, name);
+        
+        if (sex) {
+            conditions = and(conditions, eq(namesbyarea.sex, sex));
+        }
+        
+        if (state) {
+            conditions = and(conditions, eq(namesbyarea.state, state));
+        }
+
+        const result = await db
+            .select()
+            .from(namesbyarea)
+            .where(conditions)
+            .orderBy(asc(namesbyarea.year));
+
+        return NextResponse.json(result);
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { error: "Failed to fetch names" },
+            { status: 500 },
+        );
     }
-
-    const result = await db
-      .select()
-      .from(names)
-      .where(
-        sex
-          ? and(eq(names.name, name), eq(names.sex, sex))
-          : and(eq(names.name, name)),
-      )
-      .orderBy(asc(names.year));
-
-    return NextResponse.json(result);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { error: "Failed to fetch names" },
-      { status: 500 },
-    );
-  }
 }
