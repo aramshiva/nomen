7c65722be5bbe2514444ec3fb1d288b14a89ee58
Aram <me@aram.sh>
2025-03-17 19:52:22 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ interface NameData {
   year: number;
 }
 
-export function Search() {
+function Search() {
   const searchParams = useSearchParams();
   const urlName = searchParams.get("name");
   const urlSex = searchParams.get("sex");
@@ -241,3 +241,4 @@ export default function Page() {
     </Suspense>
   );
 }
+
